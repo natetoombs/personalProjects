ffa4981d0cf036dac63e28cce96267b08e376e75
Nathan Toombs <natetoombs@gmail.com>
2020-04-28 16:42:12 -0600

    Began LearnCpp Tutorials

---
+++ learncpp/1-1&1-2.cpp
@@ -0,0 +1,11 @@
+/* 1.1/1.2
+    1.2 is demonstrated in using the comments.
+*/
+
+#include <iostream> // Includes the library for using cout
+
+int main() // Defines the main function, which is run from the start
+{
+   std::cout << "Hello world!"; // Prints the phrase to the output
+   return 0; // Ends the program
+}
